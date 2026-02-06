ac1444001c469e46a0473e99794b674e0719cef0
sandbox <ghostnether28@gmail.com>
2026-02-06 20:52:03 -0300

    Organizar estrutura do projeto e organizar bug do compilador makefile

--- src/main.asm
+++ src/main.asm
@@ -1,7 +1,7 @@
 global _start
 
-%include constantes.inc                  ; inclue as constantes usadas no programa
-%include macros.inc                      ; inclue as macros usadas no programa 
+%define constantes.inc                  ; inclue as constantes usadas no programa
+%define macros.inc                      ; inclue as macros usadas no programa 
 
 section .data  
 ; ==========================================================================
@@ -20,4 +20,4 @@ _start:
     mov rsi, msg                           ; endereço da mensagem a ser escrita
     mov rdx, len                           ; comprimento da mensagem   
 
-exit 0                                 ; chamada de sistema para sair do programa, com código de saída 0 (sucesso)             
+    syscall                                ; chamada de sistema para executar a escrita
